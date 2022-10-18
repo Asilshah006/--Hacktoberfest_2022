aefc8febe45baac6a6660f8e8c485c67ac9b4a02
Majid Ali <83289212+majidali55566@users.noreply.github.com>
2022-10-17 21:32:00 -0700

    Create leapyear.java

---
+++ Java/OOP/leapyear.java
@@ -0,0 +1,17 @@
+public class CheckLeapYear {
+
+  public static void main(String[] args) {
+
+    int year;
+      System.out.println("Enter an Year :: ");
+      Scanner sc = new Scanner(System.in);
+      year = sc.nextInt();
+
+      if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0))
+         System.out.println("Specified"+year+" year is a leap year");
+      else
+         System.out.println("Specified "+year+" is not a leap year");
+   }
+  
+  }
+}

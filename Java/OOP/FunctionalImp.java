f323f57dda3f6b0c1332cb44f568fccea09cf8b0
Majid Ali <83289212+majidali55566@users.noreply.github.com>
2022-10-18 06:11:23 -0700

    Create FunctionalImp.java

---
+++ Java/OOP/FunctionalImp.java
@@ -0,0 +1,19 @@
+@FunctionalInterface
+interface Square { 
+    int calculate(int x); 
+} 
+  
+class Test { 
+    public static void main(String args[]) 
+    { 
+        int a = 5; 
+  
+        // lambda expression to define the calculate method 
+        Square s = (int x) -> x * x; 
+  
+        // parameter passed and return type must be 
+        // same as defined in the prototype 
+        int ans = s.calculate(a); 
+        System.out.println(ans); 
+    } 
+}
